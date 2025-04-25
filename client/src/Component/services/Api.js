82da4977268174357e944e6f53c53c7c1a018ee6
Rohit Nigam <153391958+RohitFSD@users.noreply.github.com>
2025-04-25 19:45:53 +0530

    Update Api.js

--- client/src/Component/services/Api.js
+++ client/src/Component/services/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:8080";
+const API_URL = "http://jobdin-production.up.railway.app";
 
 export const savePost = async (payload)=>{
     try{
